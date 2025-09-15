2ceba68c0d848145f0a98cc97eb551a06e80c08c
Malkhan-dev <hankishievmalik663@gmail.com>
2025-09-15 18:12:13 +0300

    Убрана двойная система масштабирования: только HeroStage + useFitFrame, удалены frame-fit/#frame оболочки

--- app/page.tsx
+++ app/page.tsx
@@ -74,52 +74,17 @@ export default function Home() {
   useCornerScale();
   return (
     <HeroStage offsetX={185} safeMarginY={60}>
-      <div className="absolute" style={{ left: 0, top: 0, width: 1920, height: 1080 }}>
-        {/* Вывеска */}
-        <img src="/art/вывеска.png" className="layer hero-element"
-             style={{ left: 306, top: -11, width: 1002.85, height: 362 }}
-             alt="Вывеска" />
-        {/* Повар */}
-        <img src="/art/Повар.png" className="layer hero-element"
-             style={{ left: 14, top: 22, width: 859, height: 1063 }}
-             alt="Повар" />
-
-        {/* БЛИНЧИК И */}
-        <div
-          className="layer blini-text"
-          style={{
-            position: "absolute",
-            left: 860.36,
-            top: 365.73,
-            width: 494.89,
-            height: 207.09,
-            fontSize: 120,
-            lineHeight: 0.9,
-            textAlign: "center",
-          }}
-        >
-          <div>БЛИНЧИК</div>
-          <div>И</div>
-        </div>
-
-        {/* РЮМОЧКА! */}
-        <div className="layer blini-text hero-element"
-             style={{ left: 850.91, top: 595.3, width: 540.75, height: 80.86, fontSize: '120px' }}>
-          РЮМОЧКА!
-        </div>
-
-        {/* навигация внутри кадра */}
-        <div
-          className="absolute"
-          style={{
-            width: 1280,
-            left: "50%",
-            transform: "translateX(-50%)",
-            bottom: 6
-          }}
-        >
-          <Navigation currentPage="/" />
-        </div>
+      <Hero />
+      <div
+        className="absolute"
+        style={{
+          width: 1280,
+          left: "50%",
+          transform: "translateX(-50%)",
+          bottom: 6
+        }}
+      >
+        <Navigation currentPage="/" />
       </div>
     </HeroStage>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,56 +1,38 @@
 "use client";
 
-import Navigation from "./Navigation";
-
 export default function Hero() {
   return (
-    <>
-      {/* Hero секция - вверху */}
-      <div className="frame-fit">
-        <div className="frame-offset">
-          <div id="frame">
-            <div className="content">
-              {/* Вывеска */}
-              <img src="/art/вывеска.png" className="layer hero-element"
-                   style={{ left: 306, top: -11, width: 1002.85, height: 362 }}
-                   alt="Вывеска" />
-              {/* Повар */}
-              <img src="/art/Повар.png" className="layer hero-element"
-                   style={{ left: 14, top: 22, width: 859, height: 1063 }}
-                   alt="Повар" />
-
-              {/* БЛИНЧИК И */}
-              <div
-                className="layer blini-text"
-                style={{
-                  position: "absolute",
-                  left: 860.36,
-                  top: 365.73,
-                  width: 494.89,
-                  height: 207.09,
-                  fontSize: 120,
-                  lineHeight: 0.9,
-                  textAlign: "center",
-                }}
-              >
-                <div>БЛИНЧИК</div>
-                <div>И</div>
-              </div>
-
-              {/* РЮМОЧКА! */}
-              <div className="layer blini-text hero-element"
-                   style={{ left: 850.91, top: 595.3, width: 540.75, height: 80.86, fontSize: '120px' }}>
-                РЮМОЧКА!
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="absolute" style={{ left: 0, top: 0, width: 1920, height: 1080 }}>
+      {/* Вывеска */}
+      <img src="/art/вывеска.png" className="layer hero-element"
+           style={{ left: 306, top: -11, width: 1002.85, height: 362 }}
+           alt="Вывеска" />
+      {/* Повар */}
+      <img src="/art/Повар.png" className="layer hero-element"
+           style={{ left: 14, top: 22, width: 859, height: 1063 }}
+           alt="Повар" />
+      {/* БЛИНЧИК И */}
+      <div
+        className="layer blini-text"
+        style={{
+          position: "absolute",
+          left: 860.36,
+          top: 365.73,
+          width: 494.89,
+          height: 207.09,
+          fontSize: 120,
+          lineHeight: 0.9,
+          textAlign: "center",
+        }}
+      >
+        <div>БЛИНЧИК</div>
+        <div>И</div>
       </div>
-
-      {/* Навигация - отдельно снизу */}
-      <div className="nav-slot">
-        <Navigation currentPage="/" />
+      {/* РЮМОЧКА! */}
+      <div className="layer blini-text hero-element"
+           style={{ left: 850.91, top: 595.3, width: 540.75, height: 80.86, fontSize: "120px" }}>
+        РЮМОЧКА!
       </div>
-    </>
+    </div>
   );
 }
